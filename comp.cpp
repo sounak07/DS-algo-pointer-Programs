1c75d7c370fdc3d158170dd4d029f729cddc6a26
sounak07 <sounakume@gmail.com>
2020-05-05 21:34:53 +0530

    more problems

--- comp.cpp
+++ comp.cpp
@@ -1,26 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int make_change(int coins[], int n, int money)
-{
-  int ans = 0;
-
-  while (money > 0)
-  {
-    int idx = upper_bound(coins, coins + n, money) - 1 - coins;
-    cout << coins[idx] << "+";
-    money -= coins[idx];
-    ans++;
-  }
-  cout << endl;
-  return ans;
-}
+typedef long long int ll;
 
 int main()
 {
-  int t = 39;
-  int coins[] = {1, 2, 5, 10, 20, 50, 100, 200, 500, 2000};
-  int n = 9;
+  int arr[9000];
+  int n;
 
-  cout << make_change(coins, n, t) << endl;
+  while (1)
+  {
+
+    int max_load = 0, avg = 0;
+    int rounds = 0;
+    cin >> n;
+
+    if (n == -1)
+      break;
+
+    for (int i = 0; i < n; i++)
+    {
+      cin >> arr[i];
+      avg += arr[i];
+    }
+
+    int actualPower = 0;
+    int compId = 1;
+    int balancedPower = 0;
+
+    if (avg % n != 0)
+    {
+      max_load = -1;
+    }
+    else
+    {
+      avg /= n;
+
+      for (int i = 0; i < n - 1; i++)
+      {
+        //to balance upto i part, how much power do i need to send to the 2nd part comp servers
+        actualPower += arr[i];
+        balancedPower = avg * compId;
+        max_load = max(max_load, abs(actualPower - balancedPower));
+        compId++;
+      }
+    }
+
+    cout << max_load << endl;
+  }
 }
